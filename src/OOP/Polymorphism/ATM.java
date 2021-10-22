9c7b823a8735d9ac29154c1ba169b7345f470721
Sorin <sorinmoldovan0526@gmail.com>
2021-10-22 10:31:45 -0500

    Polymorphism  practice

---
+++ src/OOP/Polymorphism/ATM.java
@@ -0,0 +1,18 @@
+package OOP.Polymorphism;
+
+public class ATM {
+
+    public void deposit(){
+        System.out.println("You are depositing from ATM");
+    }
+
+    public void withDraw (){
+        System.out.println("you can withdraw 3k from ATM");
+    }
+
+    public void balance(){
+        System.out.println("Showing balance from ATM");
+    }
+
+
+}
---
+++ src/OOP/Polymorphism/Branch.java
@@ -0,0 +1,17 @@
+package OOP.Polymorphism;
+
+public class Branch extends ATM {
+
+    public void loan (){
+        System.out.println("Branch can give you up to 100k");
+    }
+
+    public void hire (){
+        System.out.println("Branch can hire Teller");
+    }
+
+    public void assistance (){
+        System.out.println("Branch is helping");
+    }
+
+}
